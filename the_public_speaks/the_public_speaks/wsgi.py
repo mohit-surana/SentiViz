f085e1b950dcadd7ef960d73f4033ab07e52fb88
doodhwala <mohit.surana95@gmail.com>
2016-11-19 21:15:45 +0530

    Django Template

---
+++ the_public_speaks/the_public_speaks/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for the_public_speaks project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_public_speaks.settings")
+
+application = get_wsgi_application()
